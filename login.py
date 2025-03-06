bc679746bf80b1842196500b2724245fe498fc66
riyouk <ankithac9606@gmail.com>
2025-03-06 12:33:15 +0530

    adding a new function

---
+++ login.py
@@ -0,0 +1,8 @@
+def login(email,passwd):
+    print(f"the entered email is {email} , the entered password is {passwd} ")
+login("abc@gmail.com","123cs23")
+
+def logout ():
+    print("the account had loged off !")
+
+logout()
